46ff8f8024293e15d0924c68370682d9ba9989d2
ZanattaFleek <zanattafleek@gmail.com>
2022-12-07 17:23:30 -0300

    InicioAulaRevisaoContexto

---
+++ RevisaoContexto/run.bat
@@ -0,0 +1,8 @@
+@echo off
+cls
+
+cd C:\aulas\fsd0019\revisaocontexto
+
+start cmd /k npm run start
+
+rem start cmd /k json-server src\mock\db.json --p 3002 --id idEscola
---
+++ RevisaoContexto/src/Router/Router.tsx
@@ -0,0 +1,53 @@
+import React from 'react'
+
+import { createBrowserRouter } from 'react-router-dom';
+
+/*
+import Escola from '../Crud/Escola/Escola';
+import ErroAplicacao from '../Layout/ErroAplicacao';
+import LayOut from '../Layout/LayOut';
+import Login from '../Login/Login';
+*/
+
+export const router = createBrowserRouter([
+  /*
+  {
+    path: "/",
+    element: <LayOut />,
+    errorElement: <ErroAplicacao />,
+    children: [{
+      path: "escola",
+      element: <Escola />,
+      errorElement: <ErroAplicacao />
+    }]
+  },
+  {
+    path: "*",
+    element: <Login />
+  }
+  */
+
+  /*
+  {
+    path: "/",
+    element: <LayOut />,
+    errorElement: <ErroAplicacao />,
+    children: [
+      {
+        path: "cadastrocliente/:idCliente",
+        element: <CadastroCliente />,
+        errorElement: <ErroAplicacao />
+      },
+      {
+        path: "cadastrofornecedor",
+        element: <CadastroFornecedor />,
+        errorElement: <ErroAplicacao />
+      }
+    ],
+  },
+  {
+    path: "*",
+    element: <ErroNavegacao />
+  }
+  */
+]);
